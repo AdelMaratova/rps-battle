cad190b7166e21857273b00a14561830cd0572ac
Adel <adel.maratova@gmail.com>
2023-05-29 18:39:31 +0600

    added generation of random quantity of objects at random positions with size 30px x 30px

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 function moveObjects() {
     const objects = Array.from(document.getElementsByClassName("object"));
     objects.forEach(object => {
+        object.style.left = `${Math.floor(Math.random() * (window.innerWidth - 50))}px`;
+        object.style.top = `${Math.floor(Math.random() * (window.innerHeight - 50))}px`;
+
         const randomX = Math.floor(Math.random() * (window.innerWidth - 50));
         const randomY = Math.floor(Math.random() * (window.innerHeight - 50));
+
         object.style.transform = `translate(${randomX}px, ${randomY}px)`;
     });
 }
 
 window.addEventListener('load', () => {
+    const quantity = Math.floor(Math.random() * 10) + 1; // Random quantity between 1 and 10
+    const container = document.getElementById('container');
+
+    for (let i = 0; i < quantity; i++) {
+        const figureTypes = ['rock', 'paper', 'scissors'];
+        const figure = document.createElement('div');
+        figure.classList.add('object');
+        figure.classList.add(figureTypes[Math.floor(Math.random() * figureTypes.length)]);
+        container.appendChild(figure);
+    }
+
     moveObjects(); // Call moveObjects immediately after the page is loaded
     setInterval(moveObjects, 5000); // Moves objects every 5 seconds
 });
